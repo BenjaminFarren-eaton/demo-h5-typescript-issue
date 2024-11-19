14bfc39750f477601f6b6fc0a39751ec4a40af07
Ben Farren <83079012+BenjaminFarren-eaton@users.noreply.github.com>
2024-11-19 11:10:05 -0500

    Add files via upload

---
+++ Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H5TypeScriptIssue
+{
+    public class Demo
+    {
+        public void DoWork()
+        {
+            Console.WriteLine(string.Join(" ", GetMessages()));
+        }
+
+        public List<string> GetMessages()
+        {
+            return new List<string>(new string[] { "Hello", "World" });
+        }
+    }
+}
